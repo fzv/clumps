4ec264c6f363c96d223ed1e920e2c5abd4aa0653
fzv <fatima.vayani@kcl.ac.uk>
2016-12-05 16:29:31 +0000

    backup

---
+++ clumps.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+/*
+- cout
+- endl
+*/
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iterator>
+#include "sdsl/suffix_arrays.hpp"
+#include "sdsl/lcp.hpp"
+
+int main(int argc, char* argv[])
+{
+
+std::cout << "hello" << std::endl;
+
+return 0;
+
+} //END_MAIN
