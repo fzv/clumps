59216a10eb041deb83c39d71c825df9c9a0ae4fc
fzv <fatima.vayani@kcl.ac.uk>
2016-12-14 15:45:34 +0000

    parse input sorted

--- clumps.cpp
+++ clumps.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 /*
-- cout
-- endl
+* cout
+* endl
 */
 #include <string>
+/*
+* string
+*/
 #include <vector>
 #include <fstream>
+/*
+* ifstream
+*/
+#include <sstream>
+/*
+* stringstream
+*/
 #include <iterator>
 #include "sdsl/suffix_arrays.hpp"
 #include "sdsl/lcp.hpp"
@@ -13,7 +23,28 @@
 int main(int argc, char* argv[])
 {
 
-std::cout << "hello" << std::endl;
+std::cout << "Welcome to CLUMP" << std::endl;
+
+// parse input
+
+std::string textfile = argv[1];
+std::ifstream tFile(textfile);
+std::string tline;
+std::stringstream T;
+if (tFile.is_open()){
+	if (tFile.good()){
+		while (getline(tFile, tline)){
+			if (tline[0] != '>') T << tline;
+		}
+	}
+	tFile.close();
+} else {
+	std::cout << "Unable to open file." << std::endl;
+}
+std::string t = T.str();
+std::cout << t << std::endl;
+
+
 
 return 0;
 
