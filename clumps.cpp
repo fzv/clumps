a5fd40f5270dc9edd20315e76f96303af7b2d5a9
fzv <fatima.vayani@kcl.ac.uk>
2016-12-14 17:58:12 +0000

    construct tprime written but needs to be tested

--- clumps.cpp
+++ clumps.cpp
@@ -28,23 +28,74 @@ std::cout << "Welcome to CLUMP" << std::endl;
 // parse input
 
 std::string textfile = argv[1];
+//int l = argv[2];
+int l = 6;
+//int m = argv[3];
+int m = 3;
 std::ifstream tFile(textfile);
 std::string tline;
-std::stringstream T;
+std::stringstream tstream;
 if (tFile.is_open()){
 	if (tFile.good()){
 		while (getline(tFile, tline)){
-			if (tline[0] != '>') T << tline;
+			if (tline[0] != '>') tstream << tline;
 		}
 	}
 	tFile.close();
 } else {
 	std::cout << "Unable to open file." << std::endl;
 }
-std::string t = T.str();
-std::cout << t << std::endl;
+std::string t = tstream.str();
+int n = t.length();
 
+// construct suffix array of t
 
+sdsl::csa_bitcompressed<> sa;
+construct_im(sa, t, 1);
+
+// constuct longest common prefix array of t
+
+sdsl::lcp_bitcompressed<> lcp;
+construct_im(lcp, t, 1);
+
+// construct t'
+/*
+std::vector<int> tt(sa.size(), -1);
+for (int i=0; i<tt.size(); i++) std::cout << tt[i] << " "; std::cout << std::endl;
+int r = -1;
+int i = 0;
+do{
+	if ( sa[i] < (n-m) ){
+		int j = i;
+		while ( ( sa[j+1] < (n-m) ) && ( lcp[j+1] >= m ) ){
+			j++;
+		}
+		if ( j-i < l/2){
+			for (int k=i; k<=j; k++){
+				tt[sa[k]] = r+1;
+			}
+			r++;
+		}
+		i = j+1;
+	} else {
+		i++;
+	}
+} while (i != n);
+for (int i=0; i<tt.size(); i++) std::cout << tt[i] << " "; std::cout << std::endl;
+*/
+std::vector<int> tt(n-m+1, -1);
+int r = -1;
+for (int i=1; i<n+1; i++){
+	if (sa[i] < n-m+1){
+		if (lcp[i] >= m){
+			tt[sa[i]] = r;
+		} else {
+			r++;
+			tt[sa[i]] = r;
+		}
+	}
+}
+for (int i=0; i<tt.size(); i++) std::cout << tt[i] << " "; std::cout << std::endl;
 
 return 0;
 
